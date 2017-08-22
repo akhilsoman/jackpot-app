943dd967525fd75b3d936feb021ddc03a61f1cad
akhilsoman <akhil.soman@ge.com>
2017-08-23 02:39:41 +0530

    easing the bonus condition

--- server/game.js
+++ server/game.js
@@ -1,7 +1,7 @@
 // game logic
 var noOfImages = 6;
 var noOfCards  = 3;
-var bonusLimit = 55;
+var bonusLimit = 50;
 
 var getResults = function(req,res){
   var resultArray = [];
@@ -10,8 +10,8 @@ var getResults = function(req,res){
        resultArray.push(Math.floor(Math.random() * noOfImages));
     }else{
       //Logic for bonus
-      var date = new Date().getSeconds();
-      var bonus = (date * Math.random() > bonusLimit) ? 1 : 0;
+      //var date = new Date().getSeconds();
+      var bonus = (100 * Math.random() > bonusLimit) ? 1 : 0;
       resultArray.push(bonus);
     }
   }
